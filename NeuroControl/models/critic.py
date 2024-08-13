cf2b6e62ca0af5020b1f755457ff5120f2f45629
SamFatehmanesh <sam.fatehmanesh@gmail.com>
2024-08-13 21:06:26 +0000

    Modified and Reintroduced Critic Model

--- NeuroControl/models/critic.py
+++ NeuroControl/models/critic.py
@@ -10,8 +10,10 @@ import pdb
 
 
 class NeuralControlCritic(nn.Module):
-    def __init__(self, state_size, mamba_seq_size, hidden_size):
+    def __init__(self, mamba_seq_size, hidden_size):
         super(NeuralControlCritic, self).__init__()
+
+        #self.critic_loss_func = nn.MSELoss()
 
         self.mamba_seq_size = mamba_seq_size
         self.hidden_size = hidden_size
@@ -21,35 +23,36 @@ class NeuralControlCritic(nn.Module):
 
         #self.loss = nn.MSELoss()
 
-        self.mlp_in = MLP(2, state_size + 2, hidden_size, hidden_size*self.mamba_seq_size)
+        #self.mlp_in = MLP(2, state_size + 2, hidden_size, hidden_size*self.mamba_seq_size)
         self.mamba = nn.Sequential(
             Mamba2(hidden_size),
             Mamba2(hidden_size),
-            Mamba2(hidden_size),
-            Mamba2(hidden_size),
         )
-        self.pre_mlp_flat = nn.Flatten()
-        self.mlp_out = MLP(4, hidden_size*self.mamba_seq_size, hidden_size, 1)
+        self.pre_mlp_flat = nn.Flatten(start_dim=0)
+        self.mlp_out = MLP(2, hidden_size*self.mamba_seq_size, hidden_size, 1)
 
 
     def forward(self, x, steps_left, current_r):
         #pdb.set_trace()
         
-        steps_left = torch.unsqueeze(steps_left, dim=0)
-        current_r = torch.unsqueeze(current_r, dim=0)
+        # steps_left = torch.unsqueeze(steps_left, dim=0)
+        # current_r = torch.unsqueeze(current_r, dim=0)
 
-        x = torch.cat((x, steps_left, current_r), dim=1)
+        # x = torch.cat((x, steps_left, current_r), dim=1)
 
-        x = self.mlp_in(x)
+        x = x.clone()
+        x[0,:,0] = steps_left
+        x[0,:,1] = current_r
+    
 
-        x = x.view(1, self.mamba_seq_size, self.hidden_size)
+
         x = self.mamba(x)
 
         x = self.pre_mlp_flat(x)
         x = self.mlp_out(x)
 
-        return x
+        return x.view(1)
 
-    def loss(self, a, b):
-        return torch.sum(torch.square(a-b))
+    # def loss(self, a, b):
+    #     return torch.sum(torch.square(a-b))
 
