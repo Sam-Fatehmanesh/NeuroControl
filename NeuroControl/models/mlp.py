64d075c064756bd6a0dcce3599d14a72307cbe9c
SamFatehmanesh <sam.fatehmanesh@gmail.com>
2024-08-28 03:01:11 +0000

    fixed typo

--- NeuroControl/models/mlp.py
+++ NeuroControl/models/mlp.py
@@ -3,7 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 
 class MLP(nn.Module):
-    def __init__(self, , input_size, hidden_size, output_size):
+    def __init__(self, layers_num, input_size, hidden_size, output_size):
         super(MLP, self).__init__()
         self.layers = nn.Sequential()
 
