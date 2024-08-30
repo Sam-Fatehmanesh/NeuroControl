85373d028360b3888f496018437c5f917bb0dbcd
SamFatehmanesh <sam.fatehmanesh@gmail.com>
2024-08-30 00:44:35 +0000

    Fixed autoencoder

--- NeuroControl/models/autoencoder.py
+++ NeuroControl/models/autoencoder.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import pdb
 from NeuroControl.models.cnn import CNNLayer, DeCNNLayer
 from NeuroControl.models.mlp import MLP
-from NeuroControl.custom_functions.stfuncs import STMNsampler 
+from NeuroControl.custom_functions.utils import STMNsampler, symlog, symexp
 from tqdm import tqdm
 
 # def to_log_space(x, epsilon=1e-10):
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 # An autoencoder for neural video
 class NeuralAutoEncoder(nn.Module):
-    def __init__(self, frame_count, per_image_discrete_latent_size_sqrt=32, cnn_kernel_size=3):#neuron_count, frame_count):
+    def __init__(self, frame_count, per_image_discrete_latent_size_sqrt=16, cnn_kernel_size=3):#neuron_count, frame_count):
         super(NeuralAutoEncoder, self).__init__()
 
         # self.per_image_latent_size = neuron_count * frame_count
@@ -33,30 +33,25 @@ class NeuralAutoEncoder(nn.Module):
 
         self.KLloss = nn.KLDivLoss(reduction='mean') 
         self.Rloss = nn.MSELoss()
-        
+
+        self.post_cnn_encoder_size = 14**2
+
+        #self.pre_dcnn_decoder_size = 14**2
+
         # Encoder
         self.encoder = nn.Sequential(
             # 280x280 initial image input
-            CNNLayer(1, 32, cnn_kernel_size),
-            nn.MaxPool2d(5, stride=5),
+            CNNLayer(1, 16, cnn_kernel_size),
+            nn.AvgPool2d(5, stride=5),
             # Pooled down to 56x56
-            CNNLayer(32, 64, cnn_kernel_size),
-            nn.MaxPool2d(4, stride=4),
+            CNNLayer(16, 64, cnn_kernel_size),
+            nn.AvgPool2d(4, stride=4),
             # Pooled down to 14x14
-            CNNLayer(64, 256, cnn_kernel_size),
-            nn.MaxPool2d(4, stride=4),
-            # Pooled down to 3x3
-            CNNLayer(256, self.per_image_latent_size, cnn_kernel_size),
-            nn.MaxPool2d(3, stride=3),
-            # Pooled down to 1x1
+            CNNLayer(64, 1, cnn_kernel_size),
+
             nn.Flatten(),
-            MLP(2, self.per_image_latent_size, self.per_image_latent_size, self.per_image_latent_size),
-            # Flattened to self.channel_out
-            
-            # nn.Unflatten(1, (self.per_image_discrete_latent_side_size, self.per_image_discrete_latent_side_size)),
-            #nn.Softmax(dim=1),
-            # STMNsampler(),
-            # nn.Flatten(),
+            MLP(2, self.post_cnn_encoder_size, self.per_image_latent_size, self.per_image_latent_size),
+
         )
 
 
@@ -73,35 +68,31 @@ class NeuralAutoEncoder(nn.Module):
         self.decoder = nn.Sequential(
             
             MLP(2, self.per_image_latent_size, self.per_image_latent_size, self.per_image_latent_size),
-            # Start with 1x1xlatent_size
-            nn.Unflatten(1, (self.per_image_latent_size, 1, 1)),
+
+            nn.Unflatten(1, (1, self.per_image_discrete_latent_side_size, self.per_image_discrete_latent_side_size)),
+
+            DeCNNLayer(1, 128, kernel_size=4, stride=4, padding=1),
             
-            # Upsample to 4x4
-            DeCNNLayer(self.per_image_latent_size, 128, kernel_size=4, stride=4, padding=0),
+            DeCNNLayer(128, 32, kernel_size=4, stride=4, padding=0),
             
-            # Upsample to 14x14
-            DeCNNLayer(128, 32, kernel_size=4, stride=4, padding=1),
+            DeCNNLayer(32, 1, kernel_size=5, stride=5, padding=0),
             
-            # Upsample to 56x56
-            DeCNNLayer(32, 8, kernel_size=4, stride=4, padding=0),
-            
-            # Upsample to 280x280
-            DeCNNLayer(8, 1, kernel_size=5, stride=5, padding=0),
+            nn.Upsample(size=(self.image_n, self.image_n), mode='bilinear'),
         )
 
-        self.act = nn.Sigmoid()
+        #self.act = nn.Sigmoid()
 
 
     def encode(self, x):
         batch_dim = x.shape[0]
 
         x = x.view(batch_dim * self.frame_count, 1, self.image_n, self.image_n)
+
         x = self.encoder(x)
 
-        noise = torch.rand(x.shape).to(x.device) - 0.5
-        
-        x = 0.99 * x + 0.01 * noise
-        
+        # Adding noise to make non deterministic
+        x = 0.99 * x + 0.01 * (torch.rand(x.shape).to(x.device) - 0.5)
+
 
         x = x.view(batch_dim * self.frame_count * self.per_image_discrete_latent_side_size, self.per_image_discrete_latent_side_size)
         x = self.discretizer(x)
@@ -118,7 +109,6 @@ class NeuralAutoEncoder(nn.Module):
         z = self.decoder(z)
         z = z.view(batch_dim, self.frame_count, self.image_n, self.image_n)
 
-        z = self.act(z)
 
         return z
     
@@ -135,14 +125,14 @@ class NeuralAutoEncoder(nn.Module):
         x = self.decode(x)
 
 
-
+        #x = self.act(x)
 
         return x, latent
 
-    def lossFunc(self, x):
+    def loss(self, x):
         x_hat, lats = self.forward(x)
         
-        return self.KLloss(torch.log(x_hat), x) + self.Rloss(x_hat, x)
+        return self.Rloss((x_hat), ((x)))
 
 
     # def train_step(self, batch, optimizer):
