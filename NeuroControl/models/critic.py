ed81521d75bc069b2bf655d6cdebd43bab29ae37
SamFatehmanesh <sam.fatehmanesh@gmail.com>
2024-08-30 22:05:06 +0000

    Modified critic

--- NeuroControl/models/critic.py
+++ NeuroControl/models/critic.py
@@ -10,48 +10,48 @@ import pdb
 
 
 class NeuralControlCritic(nn.Module):
-    def __init__(self, mamba_seq_size, hidden_size):
+    def __init__(self, hidden_state_size, mamba_seq_size, reward_size):
         super(NeuralControlCritic, self).__init__()
 
-        #self.critic_loss_func = nn.MSELoss()
+        assert hidden_state_size % mamba_seq_size == 0, "hidden_state_size must be divisible by mamba_seq_size"
+
+        self.loss = nn.MSELoss()
 
         self.mamba_seq_size = mamba_seq_size
-        self.hidden_size = hidden_size
+        self.hidden_size = hidden_state_size // mamba_seq_size
 
         # # Assert that hidden_size is divisible by mamba_seq_size
         # assert hidden_size % mamba_seq_size == 0, "hidden_size must be divisible by mamba_seq_size"
 
         #self.loss = nn.MSELoss()
 
-        #self.mlp_in = MLP(2, state_size + 2, hidden_size, hidden_size*self.mamba_seq_size)
+        self.mlp_in = MLP(1, hidden_state_size, hidden_state_size, hidden_state_size)
         self.mamba = nn.Sequential(
             Mamba2(hidden_size),
             Mamba2(hidden_size),
         )
-        self.pre_mlp_flat = nn.Flatten(start_dim=0)
-        self.mlp_out = MLP(4, hidden_size*self.mamba_seq_size, hidden_size, 1)
+        #self.pre_mlp_flat = nn.Flatten(start_dim=0)
+        self.mlp_out = MLP(1, hidden_size*self.mamba_seq_size, hidden_size, reward_size)
 
 
-    def forward(self, x, steps_left, current_r):
-        #pdb.set_trace()
-        
-        # steps_left = torch.unsqueeze(steps_left, dim=0)
-        # current_r = torch.unsqueeze(current_r, dim=0)
+    def forward(self, x):#, steps_left, current_r):
+        batch_dim = x.shape[0]
 
-        # x = torch.cat((x, steps_left, current_r), dim=1)
+        # x = x.clone()
+        # x[:,:,0] = steps_left
+        # x[:,:,1] = current_r
 
-        x = x.clone()
-        x[0,:,0] = steps_left
-        x[0,:,1] = current_r
+
     
-
-
+        x = self.mlp_in(x)
+        
+        x = x.view(batch_dim, self.mamba_seq_size, self.hidden_size)
         x = self.mamba(x)
+        x = x.view(batch_dim, self.mamba_seq_size*self.hidden_size)
 
-        x = self.pre_mlp_flat(x)
         x = self.mlp_out(x)
 
-        return x.view(1)
+        return x
 
     # def loss(self, a, b):
     #     return torch.sum(torch.square(a-b))
