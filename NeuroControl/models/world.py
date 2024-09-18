0288324011705343e72566a59c95ca7044ef223d
SamFatehmanesh <sam.fatehmanesh@gmail.com>
2024-09-18 21:42:48 +0000

    fixed regular and imaginary forward in world.py

--- NeuroControl/models/world.py
+++ NeuroControl/models/world.py
@@ -42,7 +42,7 @@ class NeuralWorldModel(nn.Module):
         self.seq_model = NeuralSeqModel(self.hidden_state_size, self.seq_obs_latent, self.action_size, self.frames_per_obs, self.per_image_discrete_latent_size_sqrt)
         self.rep_model = NeuralRepModel(self.hidden_state_size, self.seq_obs_latent, self.frames_per_obs, self.per_image_discrete_latent_size_sqrt)
 
-        self.reward_model = NeuralControlCritic(self.hidden_state_size, self.frames_per_obs, self.frames_per_obs)
+        self.reward_model = NeuralControlCritic(self.hidden_state_size, self.frames_per_obs, self.frames_per_obs, internal_h_state_multiplier=8)
 
 
 
@@ -57,8 +57,11 @@ class NeuralWorldModel(nn.Module):
 
 
         pred_obs_lat_sample, pred_obs_lat_dist = self.rep_model.forward(hidden_state)
-        predicted_rewards_logits, _ = self.reward_model.forward(hidden_state)
+        predicted_rewards_logits, predicted_rewards_logits_ema = self.reward_model.forward(hidden_state)
         
+        decoded_obs, obs_lats_sample, obs_lats_dist = self.autoencoder(obs, hidden_state)
+        decoded_obs = decoded_obs.view(batch_dim, self.frames_per_obs, self.image_n, self.image_n)
+
         obs_lats_sample = obs_lats_sample.view(batch_dim, self.seq_obs_latent)
         
         
@@ -66,14 +69,17 @@ class NeuralWorldModel(nn.Module):
         hidden_state = self.seq_model.forward(obs_lats_sample, hidden_state, action)
 
 
-        
-
         return decoded_obs, pred_obs_lat_sample, obs_lats_sample, hidden_state, predicted_rewards_logits, predicted_rewards_logits_ema, obs_lats_dist, pred_obs_lat_dist    
+
 
     def imagine_forward(self, hidden_state, obs_latent, action):
         batch_size = hidden_state.shape[0]
 
-        predicted_reward = logits_to_reward(self.reward_model.forward(hidden_state))
+        predicted_reward_logits = self.reward_model.forward(hidden_state, ema_forward=False)
+        predicted_reward_logits = predicted_reward_logits.view(batch_size*self.frames_per_obs, self.reward_model.reward_prediction_logits_num)
+
+        predicted_reward = logits_to_reward(predicted_reward_logits).view(batch_size, self.frames_per_obs)
+         
 
         pred_obs_lat_sample, _ = self.rep_model.forward(hidden_state)
 
